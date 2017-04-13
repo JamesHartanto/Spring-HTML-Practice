1af571c36fa2da5f50db63fde3a8625295827052
James Hartanto <jameshartanto1993@gmail.com>
2017-04-13 15:28:27 -0400

    it works YAY but no game haha

--- src/main/java/com/theironyard/LoginController.java
+++ src/main/java/com/theironyard/LoginController.java
@@ -1,7 +1,11 @@
 package com.theironyard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.SQLException;
 
 /**
  * Created by JamesHartanto on 4/12/17.
@@ -9,8 +13,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    @Autowired
+    LeaderboardRepo repo;
+
     @RequestMapping("/")
     public String homePage(){
         return "index";
     }
+
+    // Hello / Welcome
+    @RequestMapping("/Welcome")
+    public String welcomePage(Model model, String name, String description, String age){
+        // Uses the input from the login page
+        model.addAttribute("name",name);
+        model.addAttribute("age",age);
+        model.addAttribute("description",description);
+
+        return "Welcome";
+    }
+
+    // Save
+    @RequestMapping("/save")
+    public String savePlayer(Player player) throws SQLException {
+        repo.insertData(player);
+        return "redirect:/results";
+    }
+
+    // Results
+    @RequestMapping("/results")
+    public String leaderboardResults(Model model) throws SQLException {
+        model.addAttribute("leaderboard",repo.listTopTen());
+        return "leaderboard";
+    }
+
 }
